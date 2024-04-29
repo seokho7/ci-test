8c57fbe68dd0836b6675e06799a9146c1fb4cee9
seokho7 <rkwhr3256@naver.com>
2024-04-29 14:38:52 +0900

    ci: 'test'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia seokho dd").listen(4000);
+const app = new Elysia().get("/", () => "Hello Elysia seokho dd ss").listen(4000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
