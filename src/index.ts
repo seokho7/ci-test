f5c2eb37f7b83b50d906d9c77f3d63f0c70f3372
seokho7 <rkwhr3256@naver.com>
2024-04-30 12:53:58 +0900

    test: 일부러 index.js 에 시크릿키 올려볼까?

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/bun", () => "Hello Elysia seokho").listen(4000);
 
+console.log(process.env)
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} `
 );
