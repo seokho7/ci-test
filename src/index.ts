ed03d572de94d33615d9e2c2d4d2a8026c084257
seokho7 <rkwhr3256@naver.com>
2024-04-30 12:54:46 +0900

    test: 일부러 index.ts 에 시크릿키 올려볼까?

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/bun", () => "Hello Elysia seokho").listen(4000);
 
-console.log(process.env)
+console.log("root_test_key_123")
+
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} `
 );
